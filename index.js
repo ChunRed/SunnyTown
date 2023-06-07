6e0f9bb740d768caa73e29dc9117d7ee013f58d3
GRed <linjack9001155@gmail.com>
2023-06-07 21:38:59 +0800

    add vue.js

---
+++ index.js
@@ -0,0 +1,39 @@
+
+Vue.createApp({
+    data: function(){
+        return{
+
+            Header:{
+
+                title: "天氣晴朗的小鎮上",
+
+                about:
+                "About With supporting text below as a natural lead-in to additional content.With supporting text below as a lead-in to additional content.With supporting text below as a natural lead-in to additional content.",
+
+                cvGRed:{
+                    
+                    name: "林俊遑 ｜ Chun-Huang, Lin",
+                    aboutCN: "國立清華大學　科技藝術研究所碩士班\n2001年出生於台灣，現就讀清華大學藝術學院科技藝術研究所。擅長互動裝置與網路藝術，關注於社會與科技工具之間的關係，尋找我們在科技環境中自身的定位。應用方面，專注於人機介面的開發與其互動實驗，透過深入技術研究去找尋其中藝術的可能。",
+                    aboutEN: "Graduate Institute of Art and Technology, National Tsing Hua University\nSpecializes in interactive installations, net art, and exploring society-technology relationship. Seeks artistic possibilities through technical.",
+                    l4: "2022.12 「TK科藝獎」 銀獎",
+                    l3: "2022.11    「洄瀾獎」 入圍",
+                    l2: "2021.7 「東京威力科創機器人大賽」 入圍",
+                    l1: "2021.1 「美國機械工程學會ASME機器人競賽」 世界第八",
+                },
+                cvJH:{
+                    
+                    l4: "2022.12 「TK科藝獎」 銀獎",
+                    l3: "2022.11    「洄瀾獎」 入圍",
+                    l2: "2021.7 「東京威力科創機器人大賽」 入圍",
+                    l1: "2021.1 「美國機械工程學會ASME機器人競賽」 世界第八",
+                },
+            },
+        }
+    },
+    methods: {
+
+    },
+    mounted: function(){
+
+    },
+}).mount('.container-fluid')

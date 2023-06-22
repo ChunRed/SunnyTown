7e41c3b75bd6ce05d39b00a17c72dc089536767f
GRed <linjack9001155@gmail.com>
2023-06-22 23:48:36 +0800

    整理版面

--- index.js
+++ index.js
@@ -1,61 +1,60 @@
 
 Vue.createApp({
-    data: function(){
-        return{
+  data: function () {
+    return {
 
-            Header:{
+      Header: {
 
-                title: "天氣晴朗的小鎮上Sunny Town",
+        title: "天氣晴朗的小鎮上Sunny Town",
 
-                aboutCN:
-                "「天氣晴朗的小鎮上」透過教育典藏兒童畫中的元素，結合創客的「交流共創」精神與新媒體「擴增實境AR」的應用，使繪畫創意不只在平面紙張上，更跳躍於空間、虛實中，跨越不同畫作，在這座天氣晴朗的小鎮上。",
-                aboutEN:
-                "\"Sunny Town\" features elements from educational children's art, combined with the creative spirit of creative entrepreneurs and the application of augmented reality (AR) through new media, making the artistic creativity not only limited to paper, but also spreading across space, virtual reality, and different artworks, in this warm and sunny town.",
+        aboutCN:
+          "「天氣晴朗的小鎮上」透過教育典藏兒童畫中的元素，結合創客的「交流共創」精神與新媒體「擴增實境AR」的應用，使繪畫創意不只在平面紙張上，更跳躍於空間、虛實中，跨越不同畫作，在這座天氣晴朗的小鎮上。",
+        aboutEN:
+          "\"Sunny Town\" features elements from educational children's art, combined with the creative spirit of creative entrepreneurs and the application of augmented reality (AR) through new media, making the artistic creativity not only limited to paper, but also spreading across space, virtual reality, and different artworks, in this warm and sunny town.",
 
-                cvGRed:{
-                    
-                    name: "林俊遑 ｜ Chun-Huang, Lin",
-                    aboutCN: 
-                    "國立清華大學　科技藝術研究所碩士班\n2001年出生於台灣，現就讀清華大學藝術學院科技藝術研究所。擅長互動裝置與網路藝術，關注於社會與科技工具之間的關係，尋找我們在科技環境中自身的定位。應用方面，專注於人機介面的開發與其互動實驗，透過深入技術研究去找尋其中藝術的可能。",
-                    aboutEN: 
-                    "Graduate Institute of Art and Technology, National Tsing Hua University\nSpecializes in interactive installations, net art, and exploring society-technology relationship. Seeks artistic possibilities through technical.",
-                    
-                },
-                cvJH:{
-                    
-                    name: "黃紀虹 ｜ Chi-Hung, HUANG",
-                    aboutCN: "國立清華大學　科技藝術研究所碩士班\n2000年出生於台灣，現就讀清華大學藝術學院科技藝術研究所。創作類型尚未定位，目前仍在學習以不同媒材呈現思維，不侷限於實體或數位方式。由於語言能力較差，將創作視為溝通語言的一種，以個人價值觀提出對現今社會現象的觀察。",
-                    aboutEN: "Graduate Institute of Art and Technology, National Tsing Hua University\nExploring different media, beyond physical or virtual methods. Using art as a communication language to observe social phenomena with personal values.",
-                    
-                },
-            },
-        }
-    },
-    methods: {
+        cvGRed: {
 
-    },
-    mounted: function(){
+          name: "林俊遑 ｜ Chun-Huang, Lin",
+          aboutCN:
+            "國立清華大學　科技藝術研究所碩士班\n2001年出生於台灣，現就讀清華大學藝術學院科技藝術研究所。擅長互動裝置與網路藝術，關注於社會與科技工具之間的關係，尋找我們在科技環境中自身的定位。應用方面，專注於人機介面的開發與其互動實驗，透過深入技術研究去找尋其中藝術的可能。",
+          aboutEN:
+            "Graduate Institute of Art and Technology, National Tsing Hua University\nSpecializes in interactive installations, net art, and exploring society-technology relationship. Seeks artistic possibilities through technical.",
 
-    },
+        },
+        cvJH: {
+
+          name: "黃紀虹 ｜ Chi-Hung, HUANG",
+          aboutCN: "國立清華大學　科技藝術研究所碩士班\n2000年出生於台灣，現就讀清華大學藝術學院科技藝術研究所。創作類型尚未定位，目前仍在學習以不同媒材呈現思維，不侷限於實體或數位方式。由於語言能力較差，將創作視為溝通語言的一種，以個人價值觀提出對現今社會現象的觀察。",
+          aboutEN: "Graduate Institute of Art and Technology, National Tsing Hua University\nExploring different media, beyond physical or virtual methods. Using art as a communication language to observe social phenomena with personal values.",
+
+        },
+      },
+    }
+  },
+  methods: {
+
+  },
+  mounted: function () {
+
+  },
 }).mount('.container-fluid')
 
-
 function IntroFunction() {
-    var x = document.getElementById("intro");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-      document.querySelector('.pictureCard').style.display = "none";
-    } else {
-      x.style.display = "none";
-      document.querySelector('.pictureCard').style.display = "block";
-    }
-  }
-
-function IntroHide() {
-    var x = document.getElementById("intro");
+  var x = document.getElementById("intro");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    document.querySelector('.pictureCard').style.display = "none";
+  } else {
     x.style.display = "none";
     document.querySelector('.pictureCard').style.display = "block";
   }
+}
+
+function IntroHide() {
+  var x = document.getElementById("intro");
+  x.style.display = "none";
+  document.querySelector('.pictureCard').style.display = "block";
+}
 
 
 //判斷是哪一個marker被掃瞄到
@@ -66,56 +65,56 @@ var marker04 = document.querySelector(".draw04");
 var marker05 = document.querySelector(".draw05");
 var marker06 = document.querySelector(".draw06");
 
-marker01.addEventListener("markerFound", (e)=>{
+marker01.addEventListener("markerFound", (e) => {
   console.log("found 01");
   pictureCard(1, true);
 })
-marker01.addEventListener("markerLost", (e)=>{
+marker01.addEventListener("markerLost", (e) => {
   console.log("lost 01");
   pictureCard(1, false);
 })
 
-marker02.addEventListener("markerFound", (e)=>{
+marker02.addEventListener("markerFound", (e) => {
   console.log("found 02");
   pictureCard(2, true);
 })
-marker02.addEventListener("markerLost", (e)=>{
+marker02.addEventListener("markerLost", (e) => {
   console.log("lost 02");
   pictureCard(2, false);
 })
 
-marker03.addEventListener("markerFound", (e)=>{
+marker03.addEventListener("markerFound", (e) => {
   console.log("found 03");
   pictureCard(3, true);
 })
-marker03.addEventListener("markerLost", (e)=>{
+marker03.addEventListener("markerLost", (e) => {
   console.log("lost 03");
   pictureCard(3, false);
 })
 
-marker04.addEventListener("markerFound", (e)=>{
+marker04.addEventListener("markerFound", (e) => {
   console.log("found 04");
   pictureCard(4, true);
 })
-marker04.addEventListener("markerLost", (e)=>{
+marker04.addEventListener("markerLost", (e) => {
   console.log("lost 04");
   pictureCard(4, false);
 })
 
-marker05.addEventListener("markerFound", (e)=>{
+marker05.addEventListener("markerFound", (e) => {
   console.log("found 05");
   pictureCard(5, true);
 })
-marker05.addEventListener("markerLost", (e)=>{
+marker05.addEventListener("markerLost", (e) => {
   console.log("lost 05");
   pictureCard(5, false);
 })
 
-marker06.addEventListener("markerFound", (e)=>{
+marker06.addEventListener("markerFound", (e) => {
   console.log("found 06");
   pictureCard(6, true);
 })
-marker06.addEventListener("markerLost", (e)=>{
+marker06.addEventListener("markerLost", (e) => {
   console.log("lost 06");
   pictureCard(6, false);
 })
@@ -125,20 +124,20 @@ marker06.addEventListener("markerLost", (e)=>{
 
 //picture card fade in & fade out 動畫
 pictureCard("", false);
-function pictureCard(index="", status = false){
-  const element =  document.querySelector('.pictureCard');
+function pictureCard(index = "", status = false) {
+  const element = document.querySelector('.pictureCard');
   const picture = document.querySelector('.painting');
-  picture.src = "painting/"+index+".jpg";
-  if(status){
-    setTimeout(function() {
-      element.classList.remove('fadeOutLeft'); 
-    }, 0); 
+  picture.src = "painting/" + index + ".jpg";
+  if (status) {
+    setTimeout(function () {
+      element.classList.remove('fadeOutLeft');
+    }, 0);
     element.classList.add('animated', 'fadeInLeft');
-     setTimeout(function() {
-      element.classList.remove('fadeInLeft'); 
-    }, 1000); 
+    setTimeout(function () {
+      element.classList.remove('fadeInLeft');
+    }, 1000);
   }
-  else{
+  else {
     element.classList.add('animated', 'fadeOutLeft');
   }
 }
